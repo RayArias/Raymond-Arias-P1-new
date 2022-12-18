4b81fe0133dbe02f2ab59f059de3ac7ecf828c4a
Ray Arias <RayArias.com@gmail.com>
2022-12-17 23:16:45 -0600

    Revision to CLI

--- CLI/src/java/com/revature/erts/models/Reimbursement.java
+++ CLI/src/java/com/revature/erts/models/Reimbursement.java
@@ -1,4 +1,163 @@
 package com.revature.erts.models;
 
+import java.sql.Timestamp;
+import java.util.UUID;
+
 public class Reimbursement {
+    static {
+        long now = System.currentTimeMillis();
+        Timestamp makeTime = new Timestamp(now);
+    }
+
+    private String id;
+    private int amount;
+    private String description;
+    private String paymentId;
+    private ReimbursementType type;
+    private ReimbursementStatus status;
+    private Timestamp submitted;
+    private String submitterId;
+    private Timestamp resolved;
+    private String resolverId;
+
+    public Reimbursement() {
+        super();
+    }
+
+    public Reimbursement(String id, int amount, String description, String paymentId, ReimbursementType type,
+                         ReimbursementStatus status, Timestamp submitted, String submitterId,
+                         String resolverId) {
+
+        long now = System.currentTimeMillis();
+        Timestamp makeTime = new Timestamp(now);
+
+        this.id          = id;
+        this.amount      = amount;
+        this.description = description;
+        this.paymentId   = paymentId;
+        this.type        = type;
+        this.status      = status;
+        this.submitted   = submitted;
+        this.submitterId = submitterId;
+        this.resolved    = makeTime;
+        this.resolverId  = resolverId;
+
+    }
+
+    public Reimbursement(String id, float amount, String description, String paymentId, String typeUUID,
+                         String statusUUID, Timestamp submitted, String submitterId,
+                         String resolverId) {
+
+        long now = System.currentTimeMillis();
+        Timestamp makeTime = new Timestamp(now);
+
+        this.id          = id;
+        this.amount      = DatatypeCrossRef.moneyFloat2Int(amount);
+        this.description = description;
+        this.paymentId   = paymentId;
+        this.type        = DatatypeCrossRef.typeUUID2Enum(typeUUID);
+        this.status      = DatatypeCrossRef.statusUUID2Enum(statusUUID);
+        this.submitted   = submitted;
+        this.submitterId = submitterId;
+        this.resolved    = makeTime;
+        this.resolverId  = resolverId;
+
+    }
+
+    public Reimbursement(String id, float amount, String description, String paymentId, String typeUUID,
+                         String statusUUID, String submitterId) {
+
+        long now = System.currentTimeMillis();
+        Timestamp makeTime = new Timestamp(now);
+
+        this.id          = id;
+        this.amount      = DatatypeCrossRef.moneyFloat2Int(amount);
+        this.description = description;
+        this.paymentId   = paymentId;
+        this.type        = DatatypeCrossRef.typeUUID2Enum(typeUUID);
+        this.status      = DatatypeCrossRef.statusUUID2Enum(statusUUID);
+        this.submitted   = makeTime;
+        this.submitterId = submitterId;
+
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void assignId() { this.id = UUID.randomUUID().toString(); }
+
+    public int getIntAmount() {
+        return this.amount;
+    }
+
+    public float getFloatAmount() { return ((float) this.amount) / 100.00F; }
+
+    public void setIntAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setFloatAmount(float amountf) { this.amount = (int) Math.floor(amountf * 100.0); }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPaymentId() { return this.paymentId; }
+
+    public void setPaymentId(String paymentId) { this.paymentId = paymentId; }
+
+    public ReimbursementType getType() { return this.type; }
+
+    public void setType(ReimbursementType type) { this.type = type; }
+
+    public ReimbursementStatus getStatus() { return this.status; }
+
+    public void setStatus(ReimbursementStatus status) { this.status = status; }
+
+    public Timestamp getSubmitted() { return this.submitted; }
+
+    public void stampSubmitted() {
+        long now = System.currentTimeMillis();
+        this.submitted = new Timestamp(now);
+    }
+
+    public String getSubmitterId() { return this.submitterId; }
+
+    public void setSubmitterId(String submitterId) { this.submitterId = submitterId; }
+
+    public Timestamp getResolved() { return this.resolved; }
+
+    public void stampResolved() {
+        long now = System.currentTimeMillis();
+        this.resolved = new Timestamp(now);
+    }
+
+    public String getResolverId() { return this.resolverId; }
+
+    public void setResolverId(String resolverId) { this.resolverId = resolverId; }
+
+    @Override
+    public String toString() {
+        return "Reimbursement{"   +
+                "id='"            + id                                       + '\'' +
+                ", amountInt="    + amount                                   +
+                ", amountFloat="  + DatatypeCrossRef.int2MoneyFloat(amount)  +
+                ", description='" + description                              + '\'' +
+                ", paymentId='"   + paymentId                                + '\'' +
+                ", typeUUID='"    + DatatypeCrossRef.typeEnum2UUID(type)     + '\'' +
+                ", typeText='"    + DatatypeCrossRef.typeEnum2Text(type)     + '\'' +
+                ", statusUUID='"  + DatatypeCrossRef.statusEnum2UUID(status) + '\'' +
+                ", statusText='"  + DatatypeCrossRef.statusEnum2Text(status) + '\'' +
+                ", submitted="    + submitted                                +
+                ", submitterId='" + submitterId                              + '\'' +
+                ", resolved="     + resolved                                 +
+                ", resolverId='"  + resolverId                               + '\'' +
+                '}';
+    }
+
 }
--- CLI/src/java/com/revature/erts/models/User.java
+++ CLI/src/java/com/revature/erts/models/User.java
@@ -1,31 +1,124 @@
 package com.revature.erts.models;
 
+import java.util.UUID;
+
 public class User {
 
-    String id;
-    String username;
-    String email;
-    String givenName;
-    String surname;
-    String password;
-    UserRole role;
-    String approvalId;
-    String authId;
-    boolean filler = true;
+    private String id;
+    private String username;
+    private String email;
+    private String givenName;
+    private String surname;
+    private String password;
+    private UserRole role;
+    private String approvalId;
+    private String authId;
+    private boolean filler = true;
 
-    User() {
+    User() {  // System Admin Info
 
-        this.id = "";
-        this.username = "";
-        this.email = "";
-        this.givenName = "";
-        this.surname = "";
-        this.password = "";
-        this.role = UserRole.ADMIN;
-        this.authId = "DEFAULT";
+        this.id         = "c26de9cb-cac3-401c-b324-3f7cd0b82845";
+        this.username   = "erts-admin";
+        this.email      = "admin@erts.net";
+        this.givenName  = "System";
+        this.surname    = "Admin";
+        this.password   = "pa55w0rd";
+        this.role       = UserRole.ADMIN;
+        this.authId     = "DEFAULT";
         this.approvalId = "DEFAULT";
 
     }
+
+    User(String id, String username, String email, UserRole role) {
+
+        this.id       = id;
+        this.username = username;
+        this.email    = email;
+        this.role     = role;
+
+    }
+
+    User(String id, String username, String email, String roleUUID) {
+
+        this.id       = id;
+        this.username = username;
+        this.email    = email;
+        this.role     = DatatypeCrossRef.roleUUID2Enum(roleUUID);
+
+    }
+
+    User(String id, String username, String email, UserRole role, String authId) {
+
+        this.id       = id;
+        this.username = username;
+        this.email    = email;
+        this.role     = role;
+        this.authId   = authId;
+
+    }
+
+    User(String id, String username, String email, String roleUUID, String authId) {
+
+        this.id       = id;
+        this.username = username;
+        this.email    = email;
+        this.role     = DatatypeCrossRef.roleUUID2Enum(roleUUID);
+        this.authId   = authId;
+
+    }
+
+    User(String id, String username, String email, String givenName, String surname, UserRole role, String authId) {
+
+        this.id        = id;
+        this.username  = username;
+        this.email     = email;
+        this.givenName = givenName;
+        this.surname   = surname;
+        this.role      = role;
+        this.authId    = authId;
+
+    }
+
+    User(String id, String username, String email, String givenName, String surname, UserRole role, String authId,
+         boolean filler, String approvalId) {
+
+        this.id         = id;
+        this.username   = username;
+        this.email      = email;
+        this.givenName  = givenName;
+        this.surname    = surname;
+        this.role       = role;
+        this.authId     = authId;
+        this.approvalId = approvalId;
+
+    }
+
+    User(String id, String username, String email, String givenName, String surname, String roleUUID, String authId,
+         boolean filler, String approvalId) {
+
+        this.id         = id;
+        this.username   = username;
+        this.email      = email;
+        this.givenName  = givenName;
+        this.surname    = surname;
+        this.role       = DatatypeCrossRef.roleUUID2Enum(roleUUID);
+        this.authId     = authId;
+        this.approvalId = approvalId;
+
+    }
+
+    User(String id, String username, String email, String givenName, String surname, String roleUUID, String authId) {
+
+        this.id        = id;
+        this.username  = username;
+        this.email     = email;
+        this.givenName = givenName;
+        this.surname   = surname;
+        this.role      = DatatypeCrossRef.roleUUID2Enum(roleUUID);
+        this.authId    = authId;
+
+    }
+
 
     User(String id, String username, String email, String givenName, String surname, String password, UserRole role,
          String authId, boolean filler, String approvalId) {
@@ -37,6 +130,34 @@ public class User {
         this.surname    = surname;
         this.password   = password;
         this.role       = role;
+        this.authId     = authId;
+        this.approvalId = approvalId;
+
+    }
+
+    User(String id, String username, String email, String givenName, String surname, String password, UserRole role,
+         String authId) {
+
+        this.id        = id;
+        this.username  = username;
+        this.givenName = givenName;
+        this.surname   = surname;
+        this.password  = password;
+        this.role      = role;
+        this.authId    = authId;
+
+    }
+
+    User(String id, String username, String email, String givenName, String surname, String password, String roleUUID,
+         String authId, boolean filler, String approvalId) {
+
+        this.id         = id;
+        this.username   = username;
+        this.email      = email;
+        this.givenName  = givenName;
+        this.surname    = surname;
+        this.password   = password;
+        this.role       = DatatypeCrossRef.roleUUID2Enum(roleUUID);
         this.authId     = authId;
         this.approvalId = approvalId;
 
@@ -54,7 +175,72 @@ public class User {
         this.role      = DatatypeCrossRef.roleUUID2Enum(roleUUID);
         this.authId    = authId;
 
-
     }
 
+    public String getId() { return this.id; }
+
+    public void assignId() { this.id = String.valueOf(UUID.randomUUID()); }
+    public String getUsername() { return this.username; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return this.email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public String getGivenName() { return this.givenName; }
+
+    public void setGivenName(String givenName) { this.givenName = givenName; }
+
+    public String getSurname() { return this.surname; }
+
+    public void setSurname(String surname) { this.surname = surname; }
+
+    public String getPassword() { return this.password; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    public UserRole getRole() { return this.role; }
+
+    public void setRole(UserRole role) { this.role = role; }
+
+    public String getRoleText() { return DatatypeCrossRef.roleEnum2Text(this.role); }
+
+    public void setRoleText(String roleText) { this.role = DatatypeCrossRef.roleText2Enum(roleText); }
+
+    public String getRoleUUID() { return DatatypeCrossRef.roleEnum2UUID(this.role); }
+
+    public void setRoleUUID(String roleUUID) { this.role = DatatypeCrossRef.roleUUID2Enum(roleUUID); }
+
+
+    public String getApprovalId() {
+        return approvalId;
+    }
+
+    public void setApprovalId(String approvalId) {
+        this.approvalId = approvalId;
+    }
+
+    public String getAuthId() {
+        return authId;
+    }
+
+    public void setAuthId(String authId) {
+        this.authId = authId;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"           +
+                "id='"           + this.id                                   + '\'' +
+                ", username='"   + this.username                             + '\'' +
+                ", email='"      + this.email                                + '\'' +
+                ", givenName='"  + this.givenName                            + '\'' +
+                ", surname='"    + this.surname                              + '\'' +
+                ", roleUUID='"   + DatatypeCrossRef.roleEnum2UUID(this.role) + '\'' +
+                ", roleText='"   + DatatypeCrossRef.roleEnum2Text(this.role) + '\'' +
+                ", approvalId='" + this.approvalId                           + '\'' +
+                ", authId='"     + this.authId                               + '\'' +
+                '}';
+    }
 }
